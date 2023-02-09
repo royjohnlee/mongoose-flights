731c9c77d8843fe11f87607337cc529a5fb8dbeb
royjohnlee <royjohnlee@DESKTOP-BO5FP47.localdomain>
2023-02-08 23:56:32 -0800

    part 5 done

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,12 +1,26 @@
 const Flight = require('../models/flight')
+
 module.exports = {
+    index,
     new: newFlight,
-    create
+    create,
+}
+function index(req, res) {
+    Flight.find({}, function (err, flights) {
+        res.render('flights/index', { flights });
+    });
 }
 
 function create(req, res) {
-    res.render('flights/')
+    const flight = new Flight(req.body)
+
+    flight.save(function (err) {
+        if (err) return res.redirect('/flights/new');
+        console.log(flight);
+        res.redirect('/flights/new');
+    });
 }
+
 function newFlight(req, res) {
     res.render('flights/new')
 }
--- routes/flights.js
+++ routes/flights.js
@@ -1,8 +1,12 @@
 var express = require('express');
+const { route } = require('.');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights')
 
 // All Routes start with `/flights`
+
+// GET /flights
+router.get('/', flightsCtrl.index)
 
 // GET /fligts/new
 router.get('/new', flightsCtrl.new)
